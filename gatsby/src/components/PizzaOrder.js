d681e3ac2da5d1201d804f61c243e11390bd93a2
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-11-30 10:04:00 +0100

    correcting bug

--- gatsby/src/components/PizzaOrder.js
+++ gatsby/src/components/PizzaOrder.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import Img from 'gatsby-image';
-import MenuItemStyles from '../styles/MenuItemStyles';
-import calculatePizzaPrice from '../utils/calculatePizzaPrice';
-import formatMoney from '../utils/formatMoney';
+import React from "react";
+import Img from "gatsby-image";
+import MenuItemStyles from "../styles/MenuItemStyles";
+import calculatePizzaPrice from "../utils/calculatePizzaPrice";
+import formatMoney from "../utils/formatMoney";
 
 const PizzaOrder = ({ order, pizzas, removeFromOrder }) => (
-  <>
-    {order.map((singleOrder, i) => {
-      const pizza = pizzas.find(
-        (singlePizza) => singlePizza.id === singleOrder.id
-      );
-      return (
-        <MenuItemStyles key={i}>
-          <Img fluid={pizza.image.asset.fluid} />
-          <h2>{pizza.name}</h2>
-          <p>
-            {singleOrder.size}{' '}
-            {formatMoney(calculatePizzaPrice(pizza.price, singleOrder.size))}
-            <button
-              type="button"
-              className="remove"
-              title={`Remove ${singleOrder.size} ${pizza.name} from order`}
-              onClick={() => removeFromOrder(index)}
-            >
-              &times;
-            </button>
-          </p>
-        </MenuItemStyles>
-      );
-    })}
-  </>
+	<>
+		{order.map((singleOrder, i) => {
+			const pizza = pizzas.find(
+				(singlePizza) => singlePizza.id === singleOrder.id
+			);
+			return (
+				<MenuItemStyles key={i}>
+					<Img fluid={pizza.image.asset.fluid} />
+					<h2>{pizza.name}</h2>
+					<p>
+						{singleOrder.size}{" "}
+						{formatMoney(calculatePizzaPrice(pizza.price, singleOrder.size))}
+						<button
+							type="button"
+							className="remove"
+							title={`Remove ${singleOrder.size} ${pizza.name} from order`}
+							onClick={() => removeFromOrder(i)}
+						>
+							&times;
+						</button>
+					</p>
+				</MenuItemStyles>
+			);
+		})}
+	</>
 );
 
 export default PizzaOrder;
--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,51 +1,54 @@
-import { graphql } from 'gatsby';
-import React from 'react';
-import Img from 'gatsby-image';
-import styled from 'styled-components';
-import SEO from '../components/SEO';
+import { graphql, Link } from "gatsby";
+import React from "react";
+import Img from "gatsby-image";
+import styled from "styled-components";
+import SEO from "../components/SEO";
 
 const PizzaGrid = styled.div`
-  display: grid;
-  grid-gap: 2rem;
-  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+	display: grid;
+	grid-gap: 2rem;
+	grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
 `;
 
 const SinglePizzaPage = ({ data: { pizza } }) => (
-  <>
-    <SEO title={pizza.name} image={pizza.image?.asset?.fluid?.src} />
-    <PizzaGrid>
-      <Img fluid={pizza.image.asset.fluid} />
-      <div>
-        <h2 className="mark">{pizza.name}</h2>
-        <ul>
-          {pizza.toppings.map((topping) => (
-            <li key={topping.id}>{topping.name}</li>
-          ))}
-        </ul>
-      </div>
-    </PizzaGrid>
-  </>
+	<>
+		<SEO title={pizza.name} image={pizza.image?.asset?.fluid?.src} />
+		<PizzaGrid>
+			<Img fluid={pizza.image.asset.fluid} />
+			<div>
+				<h2 className="mark">{pizza.name}</h2>
+				<ul>
+					{pizza.toppings.map((topping) => (
+						<li key={topping.id}>{topping.name}</li>
+					))}
+				</ul>
+				<Link to="/order">
+					<button>Buy it now</button>
+				</Link>
+			</div>
+		</PizzaGrid>
+	</>
 );
 
 export default SinglePizzaPage;
 
 export const query = graphql`
-  query($slug: String!) {
-    pizza: sanityPizza(slug: { current: { eq: $slug } }) {
-      name
-      id
-      image {
-        asset {
-          fluid(maxWidth: 800) {
-            ...GatsbySanityImageFluid
-          }
-        }
-      }
-      toppings {
-        name
-        id
-        vegetarian
-      }
-    }
-  }
+	query($slug: String!) {
+		pizza: sanityPizza(slug: { current: { eq: $slug } }) {
+			name
+			id
+			image {
+				asset {
+					fluid(maxWidth: 800) {
+						...GatsbySanityImageFluid
+					}
+				}
+			}
+			toppings {
+				name
+				id
+				vegetarian
+			}
+		}
+	}
 `;
